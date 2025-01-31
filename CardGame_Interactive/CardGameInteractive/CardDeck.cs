aef0c8a14dfaf3cb1a2a1ff0974904a505cae109
camamaga <camama@sheridancollege.ca>
2025-01-31 14:41:37 -0500

    Defined all field variables in all of the user defined types

---
+++ CardGame_Interactive/CardGameInteractive/CardDeck.cs
@@ -0,0 +1,12 @@
+namespace CardGameInteractive;
+
+/// <summary>
+/// Defines the card deck as a list of cards
+/// </summary>
+public class CardDeck
+{
+    /// <summary>
+    /// The list of cards in the deck
+    /// </summary>
+    private List<Card> _cardList;
+}
---
+++ CardGame_Interactive/CardGameInteractive/CardSuit.cs
@@ -0,0 +1,12 @@
+namespace CardGameInteractive;
+
+/// <summary>
+/// Defines the types suits that cards have
+/// </summary>
+public enum CardSuit
+{
+    Diamonds = 1,
+    Clubs,
+    Hearts,
+    Spades
+}
